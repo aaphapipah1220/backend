bc3c459544fade04b46ed9f4f3d64efac3a9062d
aaphapipah1220 <aaphapipah@outlook.com>
2025-08-15 21:51:54 +0700

    update requests show to desktop app with tkinter

--- request/test.py
+++ request/test.py
@@ -1,10 +1,39 @@
 import requests
+import json
+import tkinter
 
-url = "https://jsonplaceholder.typicode.com/posts"
-response = requests.get(url)
+api_key = "9459b7154238a873aaddf1cef6d06fdc"
+url = f"https://api.weatherstack.com/current?access_key={api_key}"
+querystring = {
+    "access_key": api_key,
+    "query": "Jakarta"
+}
+
+response = requests.get(url, querystring)
 
 if response.status_code == 200:
     data = response.json()
-    print(f"data ditemukan {data}")
+    print(json.dumps(data, indent=4))
 else:
     print(f"Gagal memuat data {response.status_code}")
+
+
+# ======================== interface
+screen = tkinter.Tk()
+screen.geometry("500x500")
+
+def check():
+    # img.configure(image=data['current']['weather_icons'])
+    show.configure(text=f"""
+                   Kota : {data['request']['query']} 
+                   Suhu : {data['current']['temperature']}
+                   Keterangan : {data['current']['weather_descriptions']}""")
+
+button = tkinter.Button(screen, text="weather check", command=check)
+show = tkinter.Label(screen)
+img = tkinter.Label(screen)
+
+button.pack()
+show.pack()
+
+screen.mainloop()
